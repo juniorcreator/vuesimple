b732496fda9e5059f0397ae8982a46f10fd70e25
Serhii Kovalenko <serhii.kovalenko@otakoyi.com>
2018-12-12 18:51:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app" class="wrapper">
+    <div class="container">
+      <h1>Main component </h1>
+      <blogs></blogs>
+    </div>
+  </div>
+</template>
+
+<script>
+  import blogs from "./Blogs.vue"
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  },
+  created () {
+
+  },
+  methods: {
+
+  },
+  components: {
+    blogs: blogs,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  html, body {
+    height: 100%;
+  }
+
+</style>
---
+++ src/Blogs.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="blogs">
+        <button @click="loaadData">Load data</button>
+        <button @click="deleteData">Del data</button>
+        <div v-for="post in posts" :key="post.id" class="blogs_items" :id="'post'+post.id">
+            <h1 class="blogs_items_title">{{post.title}}</h1>
+            <p class="blogs_items_text"> {{post.body}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from  'axios'
+  export default {
+    data () {
+      return {
+        posts: [],
+      }
+    },
+    created () {
+
+    },
+    methods: {
+      loaadData () {
+        axios.get('https://jsonplaceholder.typicode.com/posts/')
+          .then(response => {
+            this.posts = response.data;
+          }).catch(error => {
+          console.error(error);
+        })
+      },
+      deleteData () {
+        this.posts = [];
+      },
+    },
+
+  }
+</script>
+
+<style scoped>
+
+</style>
